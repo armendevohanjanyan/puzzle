3be0755688b540d064b948e46279830b6e2d3c3a
Karen Ohanjanyan <karen@iMac-Karen.local>
2025-09-04 11:11:35 +0400

    first commit

---
+++ media/text/strings.js
@@ -0,0 +1,44 @@
+var _STRINGS = {
+	"Ad":{
+		"Mobile":{
+			"Preroll":{
+				"ReadyIn":"The game is ready in ",
+				"Loading":"Your game is loading...",
+				"Close":"Close",
+			},
+			"Header":{
+				"ReadyIn":"The game is ready in ",
+				"Loading":"Your game is loading...",
+				"Close":"Close",
+			},
+			"End":{
+				"ReadyIn":"Advertisement ends in ",
+				"Loading":"Please wait ...",
+				"Close":"Close",
+			},								
+		},
+	},
+	
+	"Splash":{
+		"Loading":"Loading ...",	
+		"LogoLine1":"Some text here",
+		"LogoLine2":"powered by MarketJS",
+		"LogoLine3":"none",		
+        "TapToStart":"TAP TO START",				
+	},
+
+	"Game":{
+		"On": "ON",
+		"Off": "OFF",
+		"Timer": "TIMER",
+		"Paused": "PAUSED",
+		"Complete": "PUZZLE<br>COMPLETE!",
+		"Time": "Time",
+		"BestTime": "Best Time",
+		"Settings": "SETTINGS"
+	},
+
+	"Results":{
+		"Title":"High score",
+	},	
+};
